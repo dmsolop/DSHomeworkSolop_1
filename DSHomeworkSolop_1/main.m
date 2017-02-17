28aa46b78cf394535767b2ea4f3d2a04f980e1e4
Дмитрий Солоп <dmsolop@ya.ru>
2017-02-17 03:42:22 +0200

    Initial Commit

---
+++ DSHomeworkSolop_1/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  DSHomeworkSolop#1
+//
+//  Created by Дмитрий Солоп on 16.02.17.
+//  Copyright © 2017 Dimon Solop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSArray+DSExtention.h"
+#import "DSProtocolOfFood.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSArray *arrayOfCreature = [NSArray array];
+        arrayOfCreature = [arrayOfCreature makeArrayOfCreature];
+        
+        NSArray *sortedArrayOfCreature = [NSArray array];
+        sortedArrayOfCreature = sortedArrayOfCreature.sortedArray;
+        
+        [arrayOfCreature printArray:arrayOfCreature];
+        [sortedArrayOfCreature printArray:sortedArrayOfCreature];
+    }
+    return 0;
+    
+}
